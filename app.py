9bd04a0e0a40e36cc6e7e8494921384a70f8146d
Michał Kubicz <mqbicz@gmail.com>
2025-03-16 16:31:07 +0100

    add HH:MM

--- app.py
+++ app.py
@@ -10,20 +10,20 @@ def index():
     return render_template('index.html', trainings=trainings)
 
 @app.route('/add', methods=['GET', 'POST'])
-def add_event():
+def add_training():
     if request.method == 'POST':
         name = request.form.get('name')
         date = request.form.get('date')
 
         try:
-            date_obj = datetime.strptime(date, '%Y-%m-%d')
+            date_obj = datetime.strptime(date, '%Y-%m-%dT%H:%M')
         except ValueError:
-            return "Invalid date format! Use YYYY-MM-DD", 400
+            return "Invalid date format! Use YYYY-MM-DD HH:MM", 400
 
         trainings.append({'name': name, 'date': date_obj})
         return redirect(url_for('index'))
 
-    return render_template('add_event.html')
+    return render_template('add_training.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
